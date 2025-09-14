0cc1013fd2b3443a8acf3ea947ae5095b4b400b2
Aktuganov Konstantin <aktuganovkonstantin@gmail.com>
2025-09-14 15:19:12 +0800

    Авторизация в админке закрыта по IP

--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,28 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Illuminate\Http\Request;
 
-// Админ-панель (должна быть ПЕРЕД основным приложением)
-Route::prefix('admin')->group(function () {
-    Route::get('/', function () {
-        return view('admin-panel');
+function getUserIp(): ?string
+{
+    $request = app(Request::class);
+
+    return $request->getClientIp();
+}
+
+if (getUserIp() === env('ADMIN_IP')) {
+    // Админ-панель (должна быть ПЕРЕД основным приложением)
+    Route::prefix('admin')->group(function () {
+        Route::get('/', function () {
+            return view('admin-panel');
+        });
+
+        Route::get('/{any}', function () {
+            return view('admin-panel');
+        })->where('any', '.*');
     });
-    
-    Route::get('/{any}', function () {
-        return view('admin-panel');
-    })->where('any', '.*');
-});
+}
+
 
 // Основное приложение
 Route::get('/', function () {
